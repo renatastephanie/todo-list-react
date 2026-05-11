ab87c892848b0edc218aa106f96cf2ff3587d1a3
renatastephanie <renatastephaniesantos@gmail.com>
2026-05-11 18:45:48 -0300

    Primeiro commit

---
+++ front-end/src/App.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { InputAdd } from "./components/InputAdd";
+
+export function App() {
+  const [list, setList] = useState([
+    { id: "1", label: "Fazer café", complete: false },
+    { id: "2", label: "Fazer café", complete: false },
+    { id: "3", label: "Fazer almoço", complete: false },
+    { id: "4", label: "Fazer janta", complete: false },
+  ]);
+
+  const handleAdd = (value: string) => {
+    setList([
+      ...list,
+      {
+        id: (list.length + 1).toString(),
+        complete: false,
+        label: value,
+      },
+    ]);
+  }
+
+  return (
+    <>
+      <div>
+        <InputAdd onAdd={handleAdd} />
+        
+        <ol>
+          {list.map((listItem) => (
+            <li key={listItem.id}>
+              {listItem.label}
+
+              {listItem.complete ? "Concluido" : ""}
+
+              <button
+                onClick={() =>
+                  setList([
+                    ...list.map((item) => ({
+                      ...item,
+                      complete: item.id === listItem.id ? true : item.complete,
+                    })),
+                  ])
+                }>
+                Concluir
+              </button>
+              <button
+                onClick={() =>
+                  setList([...list.filter((item) => item.id !== listItem.id)])
+                }>
+                Remover
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </>
+  );
+}
---
+++ front-end/src/components/InputAdd.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+interface IInputAddProps {
+  onAdd: (value: string) => void
+}
+
+export const InputAdd = (props: IInputAddProps) => {
+  const [value, setValue] = useState("");
+
+  const handleAdd = () => {
+    props.onAdd(value);
+    setValue("");
+  }
+
+  return (
+    <div>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+
+      <button onClick={handleAdd}>
+        Adicionar
+      </button>
+    </div>
+  );
+}
