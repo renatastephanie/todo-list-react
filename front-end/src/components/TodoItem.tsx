1604469df36f4475e72b12de31c7ff2989ee1709
renatastephanie <renatastephaniesantos@gmail.com>
2026-05-12 15:08:43 -0300

    fix: Criação do componente TodoItem.tsx

---
+++ front-end/src/components/TodoItem.tsx
@@ -0,0 +1,22 @@
+
+interface ITodoItemProps {
+  id: string;
+  label: string;
+  complete: boolean;
+  onRemove(): void;
+  onComplete(): void;
+}
+
+export const TodoItem = ({ id, label, complete, onComplete, onRemove }: ITodoItemProps) => {
+
+  return (
+    <li key={id}>
+      {label}
+
+      {complete ? "Concluido" : ""}
+
+      <button onClick={onComplete}>Concluir</button>
+      <button onClick={onRemove}>Remover</button>
+    </li>
+  );
+};
