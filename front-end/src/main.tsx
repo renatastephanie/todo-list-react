379668ba29b49a978a2fd00361de6e5cf6b2bfad
renatastephanie <renatastephaniesantos@gmail.com>
2026-05-14 22:20:51 -0300

    feat: Comenta o miragejs

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import './mocks/server.ts';
+// import './mocks/server.ts';
 import { App } from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
