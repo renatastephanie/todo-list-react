93f6ee047f9ebfca5e535486c4f6a757a74b4dc7
renatastephanie <renatastephaniesantos@gmail.com>
2026-05-14 22:19:39 -0300

    feat: adiciona AppLayout com navegação via NavLink

--- front-end/src/shared/layout/AppLayout.tsx
+++ front-end/src/shared/layout/AppLayout.tsx
@@ -1,11 +1,13 @@
+import { NavLink } from 'react-router-dom';
 import './AppLayout.scss';
 
 export const AppLayout = ({ children }: React.PropsWithChildren) => {
   return (
     <div className="layout-base">
       <div className='layout-header'>
-        <a>Página Inicial</a>
-        <a>Sobre</a>
+        <NavLink to='/'>Página Inicial</NavLink>
+        <NavLink to='/concluidas'>Concluídas</NavLink>
+        <NavLink to='/pendentes'>Pendentes</NavLink>
       </div>
 
       <hr className='layout-divider' />
