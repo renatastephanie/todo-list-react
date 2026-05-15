23d846723603632082410284e28717cac8ac4a61
renatastephanie <renatastephaniesantos@gmail.com>
2026-05-14 22:19:03 -0300

    feat: adiciona rotas com react-router-dom

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,22 @@
-import { Home } from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppLayout } from "./shared/layout/AppLayout";
+import { Home } from "./pages/Home";
+import { Concluded } from './pages/Concluded';
+import { Pending } from './pages/Pending';
+
 
 export function App() {
   return (
     <>
-      <AppLayout>
-        <Home />
-      </AppLayout>
+      <BrowserRouter>
+        <AppLayout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/concluidas" element={<Concluded />} />
+            <Route path="/pendentes" element={<Pending />} />
+          </Routes>
+        </AppLayout>
+      </BrowserRouter>
     </>
   );
 }
