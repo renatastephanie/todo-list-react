8aadbb56933331b375b534ef9fcdb4401083a438
renatastephanie <renatastephaniesantos@gmail.com>
2026-05-14 22:17:15 -0300

    feat: adiciona serviço TodoAPI com axios e métodos CRUD

--- front-end/src/shared/services/api/TodoAPI.ts
+++ front-end/src/shared/services/api/TodoAPI.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3333",
+});
 
 export interface ITodo {
   id: string;
